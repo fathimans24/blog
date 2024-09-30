6cd488dd48611d9e51880e7061a817771997dcf4
fathimans24 <160525090+fathimans24@users.noreply.github.com>
2024-09-30 12:10:32 +0530

    initial commit

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 //Write missing code here for database connection
-const mongoDB_URL = "mongodb+srv://SubaSreeRamesh01:subasree1999@cluster0.61hpy.mongodb.net/blogApp?retryWrites=true&w=majority&appName=Cluster0";
+const mongoDB_URL = "mongodb+srv://Fathimans:fathima123@cluster0.fpl6f.mongodb.net/blogdata?retryWrites=true&w=majority&appName=Cluster0";
 require('dotenv').config()
 mongoose.connect(mongoDB_URL).then(()=>{
     console.log('connection established');
